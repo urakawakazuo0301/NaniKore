beafeb7f1565ddeed500dbace538775e39837f44
urakawakazuo0301 <urakawakazuo0301@gmail.com>
2024-09-04 19:01:33 +0900

    ビュー作成

--- app/models/user.rb
+++ app/models/user.rb
@@ -4,6 +4,13 @@ class User < ApplicationRecord
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :validatable
 
+  has_many :items
+  
   validates :nickname, presence: true
+
+  VALID_PASSWORD_REGEX =/\A(?=.*[a-zA-Z])(?=.*\d)[a-zA-Z\d]{6,}\z/
+  validates :password, presence: true,
+            format: { with: VALID_PASSWORD_REGEX,
+             message: "は英数字6文字以上が必要です"}, on: :create
 
 end
