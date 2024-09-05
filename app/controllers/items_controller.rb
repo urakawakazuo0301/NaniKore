59d6c90a211b172a88d711b7dbf40367ccb63818
urakawakazuo0301 <urakawakazuo0301@gmail.com>
2024-09-05 18:19:12 +0900

    active_hash導入

--- app/controllers/items_controller.rb
+++ app/controllers/items_controller.rb
@@ -8,7 +8,19 @@ class ItemsController < ApplicationController
   end
 
   def create
+    @item = Item.new(item_params)
+    if @item.save
+      redirect_to '/'
+    else
+      render 'new', status: :unprocessable_entity
+    end
   end
 
+
+  private
+
+  def item_params
+    params.require(:item).permit(:name, :category_id, :quantity_id, :notes, :color, :image).merge(user_id: current_user.id)
+  end
 
 end
