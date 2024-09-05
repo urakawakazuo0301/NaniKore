b06810df618f2e4a07de4308a5c69fb8e03a66a1
urakawakazuo0301 <urakawakazuo0301@gmail.com>
2024-09-05 12:38:05 +0900

    Active Storageの導入

--- Gemfile
+++ Gemfile
@@ -80,3 +80,6 @@ end
 gem 'pry-rails'
 
 gem 'devise'
+
+gem 'mini_magick'
+gem 'image_processing', '~> 1.2'
--- app/models/item.rb
+++ app/models/item.rb
@@ -1,4 +1,9 @@
 class Item < ApplicationRecord
-  
+
   belongs_to :user
+  has_one_attached :image
+
+  validates :image, presence: true
+  validates :name, presence: true
+  
 end
