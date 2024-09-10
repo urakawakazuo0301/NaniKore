4f7106338bad92cf99225bfb57c6ae87024420c0
urakawakazuo0301 <urakawakazuo0301@gmail.com>
2024-09-11 00:01:04 +0900

    アイコンの追加／実装

--- app/javascript/controllers/application.js
+++ app/javascript/controllers/application.js
@@ -1,9 +1,7 @@
-import { Application } from "@hotwired/stimulus"
+import { Application } from "@hotwired/stimulus";
 
-const application = Application.start()
+const application = Application.start();
+application.debug = false;
+window.Stimulus   = application;
 
-// Configure Stimulus development experience
-application.debug = false
-window.Stimulus   = application
-
-export { application }
+export { application };
