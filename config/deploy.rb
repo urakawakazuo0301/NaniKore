946ea7534d6030b8c4656d0898336d85da9a86d9
urakawakazuo0301 <urakawakazuo0301@gmail.com>
2024-10-03 16:04:50 +0900

    自動デプロイの準備２

--- config/deploy.rb
+++ config/deploy.rb
@@ -16,7 +16,7 @@ set :rbenv_ruby, '3.2.0'
 
 # どの公開鍵を利用してデプロイするか
 set :ssh_options, auth_methods: ['publickey'],
-                                  keys: ['my-key-pair.pem'] 
+                                  keys: ['~/.ssh/my-key-pair.pem'] 
 
 # プロセス番号を記載したファイルの場所
 set :unicorn_pid, -> { "#{shared_path}/tmp/pids/unicorn.pid" }
