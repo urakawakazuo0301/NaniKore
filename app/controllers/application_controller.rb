b31638dce4ab6b784daaa1f5f06eb7d20375aa21
urakawakazuo0301 <urakawakazuo0301@gmail.com>
2026-07-17 18:17:58 +0900

    実装：Basic認証をcredentials参照に変更

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,6 +1,5 @@
 class ApplicationController < ActionController::Base
-
-  include Pundit::Authorization 
+  include Pundit::Authorization
 
   before_action :basic_auth
   before_action :configure_permitted_parameters, if: :devise_controller?
@@ -10,13 +9,13 @@ class ApplicationController < ActionController::Base
   def after_sign_in_path_for(resource)
     root_path
   end
-  
+
   private
 
   def user_not_authorized
-    flash[:alert] = "この操作を行う権限がありません。"
+    flash[:alert] = 'この操作を行う権限がありません。'
     flash[:pundit_error] = true
-    
+
     if user_signed_in?
       redirect_to(request.referrer || root_path)
     else
@@ -26,7 +25,8 @@ class ApplicationController < ActionController::Base
 
   def basic_auth
     authenticate_or_request_with_http_basic do |username, password|
-      username == ENV['BASIC_AUTH_USER'] && password == ENV['BASIC_AUTH_PASSWORD']
+      username == Rails.application.credentials.dig(:basic_auth, :user) &&
+        password == Rails.application.credentials.dig(:basic_auth, :password)
     end
   end
 
@@ -34,5 +34,4 @@ class ApplicationController < ActionController::Base
     devise_parameter_sanitizer.permit(:sign_up, keys: [:nickname])
     devise_parameter_sanitizer.permit(:account_update, keys: [:nickname])
   end
-
 end
